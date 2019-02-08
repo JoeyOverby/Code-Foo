c41a82d5508f40ace0a13638d9468bd86a680d05
Joey Overby <38049004+JoeyOverby@users.noreply.github.com>
2019-02-08 14:06:03 -0700

    All good, except need to figure out how to post or pre process the excess row around everything

--- node/islandPerimeter/findPerimeter.js
+++ node/islandPerimeter/findPerimeter.js
@@ -14,6 +14,56 @@ class PerimeterFinder{
 		var height = input.length;
 		var width = input[0].length;
 
+		//Initialize an array that will be the sum of the number of land boxes around it.
+		var numNeighbors = new Array(height+2).fill(0).map(() => new Array(width+2).fill(0));
+		
+
+		for(var heightIndex = 0; heightIndex < height; heightIndex++){
+			numNeighborsHeightIndex =heightIndex+1;
+			for(var widthIndex=0; widthIndex < width; widthIndex++){
+				numNeighborsWidthIndex = widthIndex + 1; //We need to use this math so that we don't go out of bounds
+
+				//If I'm a 1 or greater, add 1 to the 4 boxes around me
+				if(input[heightIndex][widthIndex] === 1){
+					numNeighbors[numNeighborsHeightIndex+1][numNeighborsWidthIndex] = numNeighbors[numNeighborsHeightIndex+1][numNeighborsWidthIndex] + 1;
+					numNeighbors[numNeighborsHeightIndex-1][numNeighborsWidthIndex] = numNeighbors[numNeighborsHeightIndex-1][numNeighborsWidthIndex] + 1;
+					numNeighbors[numNeighborsHeightIndex][numNeighborsWidthIndex+1] = numNeighbors[numNeighborsHeightIndex][numNeighborsWidthIndex+1] + 1;
+					numNeighbors[numNeighborsHeightIndex][numNeighborsWidthIndex-1] = numNeighbors[numNeighborsHeightIndex][numNeighborsWidthIndex-1] + 1;
+				}
+
+				console.log(heightIndex + ","+ widthIndex + " = " + input[heightIndex][widthIndex] + " ");
+			}
+			console.log("");
+		}
+
+		//Print the neighbors count array
+		for(var numNeighborsHeightIndex = 1; numNeighborsHeightIndex < height+1; numNeighborsHeightIndex++){
+			var rowString = "";
+			for(var numNeighborsWidthIndex=1; numNeighborsWidthIndex < width+1; numNeighborsWidthIndex++){
+				rowString += numNeighbors[numNeighborsHeightIndex][numNeighborsWidthIndex] + " ";
+			}
+			console.log(rowString);
+		}
+
+		//Now run through the array and add up the number of neighbors and apply the math.
+		var perimeter = 0;
+		for(var numNeighborsHeightIndex = 1; numNeighborsHeightIndex < height+1; numNeighborsHeightIndex++){
+			for(var numNeighborsWidthIndex=1; numNeighborsWidthIndex < width+1; numNeighborsWidthIndex++){
+				switch(numNeighbors[numNeighborsHeightIndex][numNeighborsWidthIndex]){
+					case 1:
+					perimeter += 3
+					break;
+					case 2:
+					perimeter +=2
+					case 3: 
+					perimeter +=1
+					default:
+					break;
+				}
+			}
+			console.log(rowString);
+		}
+
 		console.log("Perimeter is: ");
 	}
     
